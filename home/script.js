cc2b0d82345ac1ad3b511dd7b8a27421decf6fe0
SamitChaubal <145633069+SamitChaubal@users.noreply.github.com>
2024-02-16 22:00:52 -0600

    fixed topnav and removed responsive topnav

--- home/script.js
+++ home/script.js
@@ -1,11 +1,10 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function responsiveNavBar(y) {
+function responsiveNavBar(x) {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-  y.classList.toggle("change");
 }
 
